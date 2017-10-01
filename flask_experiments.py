81ca277e3eb688759f66d36424eb1fcbe3b119ca
stevemolloy <sdmolloy@gmail.com>
2017-10-01 19:18:22 +0200

    New route

--- flask_experiments.py
+++ flask_experiments.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def main_controller():
     return 'Hello World!'
+
+
+@app.route('/quad')
+def quad_controller():
+    return 'Quad page'
 
 
 if __name__ == '__main__':
