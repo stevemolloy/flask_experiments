e89ebfb459bd58116aaa9f139a33881683a828c5
stevemolloy <sdmolloy@gmail.com>
2017-10-01 23:08:39 +0200

    Lots of changes

--- flask_experiments.py
+++ flask_experiments.py
@@ -13,5 +13,15 @@ def quad_controller():
     return render_template('quadrupole.html')
 
 
+@app.route('/acceleration/numerical')
+def acc_num_controller():
+    return render_template('acc_num.html')
+
+
+@app.route('/acceleration/model')
+def acc_model_controller():
+    return render_template('acc_model.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
